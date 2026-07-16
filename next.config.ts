591d6df36b619203f53f7ed77f3957131054497a
openmymai <maicmi@me.com>
2026-07-16 16:27:17 +0700

    Update Score Font size in Live Page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["badminton5gear.win", "192.168.0.7", "localhost", "192.168.1.10"],
+  allowedDevOrigins: ["badminton5gear.win", "192.168.0.7", "localhost", "192.168.1.10", "192.168.1.45"],
 };
 
 export default nextConfig;
